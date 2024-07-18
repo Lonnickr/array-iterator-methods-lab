d2e1ee0ca40e71d9a38dc34f8fc368fe3104bc17
Lonnickr <lonnickr@gmail.com>
2024-07-18 16:23:26 -0400

    done

--- script.js
+++ script.js
@@ -78,4 +78,76 @@ console.log('Exercise 2 My Result: ', inventorNames)
 
 // 3. Sort the inventors by birth date in ascending order (from those born furthest in the past to those born most recently).
 
-let sortedByBirthYear = [] 
+let sortedByBirthYear = inventors.sort((a,b) => a.year - b.year); 
+console.log('Exercise 3 My Result: ', sortedByBirthYear)
+
+// Array.prototype.find()
+
+// 4. Use Array.prototype.find() to identify the inventor with the first name 'Ada' from an array of inventor objects
+
+// - You have an array of objects, each representing an inventor with various properties including their first name.
+// -  Utilize the Array.prototype.find() method to locate the object for the inventor named 'Ada'.
+// -  Assign the found inventor object to the variable 'inventorNamedAda'
+
+let inventorNamedAda = inventors.find(inventor => inventor.first === 'Ada');
+console.log('Exercise 4 My Result: ', inventorNamedAda);
+
+// Array.prototype.map()
+
+// 5. Use the Array.prototype.map() method to reformat each name in the 'people' array. 
+// The goal is to convert names from "Last, First" format to "First Last" format.
+
+// Hint: Use the String.prototype.split method to separate the first and last names. You can split the string using ', ' as the separator.
+// After splitting the names, rearrange them to the "First Last" format.
+
+
+let firstLast = people.map(person => {
+    let [last, first] = person.split(',');
+    return `${first} ${last}`;
+});
+
+console.log('Exercise 5 My Result: ', firstLast);
+
+// Array.prototype.some()
+
+// 6. Determine if there is at least one person in the devs array who is 18 years old or older.
+
+// - You have an array of people with their respective ages.
+// - Use the Array.prototype.some() method to check if any person in the array is 18 years old or older.
+// - Store the result (true or false) in the variable 'isAdultPresent'. 
+
+let isAdultPresent = devs.some(dev => dev.age = 18);
+
+console.log('Exercise 6 My Result: ', isAdultPresent);
+
+// Array.prototype.every()
+
+// 7. Use Array.prototype.every() to determine if every person in the devs array is 19 years old or older.
+
+// - You have an array of individuals, each with their year of birth represented by the 'year' property.
+// - Use the Array.prototype.every() method to verify if every individual in the array is at least 19 years old.
+// - Store the result (true or false) in the variable 'isEveryone19OrOlder'.
+
+let isEveryone19OrOlder = devs.every(dev => dev.age >= 19);
+
+console.log('Exercise 7 My Result: ', isEveryone19OrOlder);
+
+
+// Array.prototype.find()
+
+// 8. Use Array.prototype.find() to identify and retrieve the comment object with a specific ID 823423 from an array of comment objects.
+
+// - Assign the found comment object to the variable 'commentById'.
+
+let commentById = comments.find(comment => comment.id === 823423);
+
+console.log('Exercise 8 My Result: ', commentById);
+
+// Array.prototype.findIndex()
+
+// 9. Determine the index of the comment that has a specific ID 123523 in an array of comment objects.
+
+// - Store the index in the variable 'idx'.
+
+let idx = comments.findIndex(comment => comment.id === 123523);
+console.log('Exercise 9 My Result: ', idx)
